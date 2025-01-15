754bae4961564fd155589dfae8102a2d8cb75aae
greenyjwk <greenyjwk@gmail.com>
2025-01-15 08:10:58 -0700

    MODIFY: modified stacking module

--- src/create_3ch.py
+++ src/create_3ch.py
@@ -2,55 +2,63 @@ import nibabel as nib
 import numpy as np
 import os
 import glob
+import json
+import argparse
 
-# VALDO
-# root_path = "/mnt/storage/Ji/brain_mri_valdo_mayo/bias-field-correction"
-# output_dir = "/mnt/storage/ji/VALDO_preprocessing/final"
+def main():
+    parser = argparse.ArgumentParser(description="Create 3-channel NIfTI images")
+    parser.add_argument('--root_path', type=str, required=False, default="/media/Datacenter_storage/Ji/brain_mri_valdo_mayo/mayo_bias_field_correction")
+    parser.add_argument('--output_dir', type=str, required=False, default="/media/Datacenter_storage/Ji/brain_mri_valdo_mayo/mayo_stacked")
+    parser.add_argument('--dataset', type=str, choices=['mayo', 'valdo'], default='mayo', required=False)
+    parser.add_argument('--config_path', type=str, required=False, default="/media/Datacenter_storage/Ji/VALDO_brain_MRI_dataset_preprocessing/configs/config.json")
+    args = parser.parse_args()
 
-# Mayo
-root_path = "/media/Datacenter_storage/Ji/brain_mri_valdo_mayo/bias-field-correction"
-output_dir = "/media/Datacenter_storage/Ji/brain_mri_valdo_mayo/mayo_stacked"
-
-
-
-if not os.path.exists(output_dir):
-    os.mkdir(output_dir)
-
-for uid in os.listdir(root_path):
-    print(uid)
-    subdir_path = os.path.join(root_path, uid)
+    root_path = args.root_path
+    output_dir = args.output_dir
+    dataset = args.dataset
+    config_path = args.config_path
     
-    # Search for NIfTI files in the subdirectory
-    nifti_files = glob.glob(os.path.join(subdir_path, "*.nii.gz"))
-    
-    if len(nifti_files) != 3:
-        print(f"Expected 3 NIfTI files in {subdir_path}, but found {len(nifti_files)}")
-        continue
+    # config file
+    with open(config_path, 'r') as config_file:
+        config = json.load(config_file)
 
-    # Sort the files to ensure consistent order (optional)
-    nifti_files.sort()
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
 
-    # creating subdirectory in output directory
-    if not os.path.exists(os.path.join(output_dir, uid)):
-        os.mkdir(os.path.join(output_dir, uid))
+    for uid in os.listdir(root_path):
+        subdir_path = os.path.join(root_path, uid)
+        
+        # creating subdirectory in output directory
+        if not os.path.exists(os.path.join(output_dir, uid)):
+            os.mkdir(os.path.join(output_dir, uid))
 
-    # Load the three NIfTI files
-    nii1 = nib.load(nifti_files[0])
-    nii2 = nib.load(nifti_files[1])
-    nii3 = nib.load(nifti_files[2])
+        if dataset == 'mayo':
+            nii1 = glob.glob(os.path.join(subdir_path, f"{config['mayo_stack_order'][0]}*.nii.gz"))
+            nii2 = glob.glob(os.path.join(subdir_path, f"{config['mayo_stack_order'][1]}*.nii.gz"))
+            nii3 = glob.glob(os.path.join(subdir_path, f"{config['mayo_stack_order'][2]}*.nii.gz"))
+        elif dataset == 'valdo':
+            nii1 = glob.glob(os.path.join(subdir_path, f"{config['valdo_stack_order'][0]}*.nii.gz"))
+            nii2 = glob.glob(os.path.join(subdir_path, f"{config['valdo_stack_order'][1]}*.nii.gz"))
+            nii3 = glob.glob(os.path.join(subdir_path, f"{config['valdo_stack_order'][2]}*.nii.gz"))
 
-    # Extract data arrays
-    data1 = nii1.get_fdata()
-    data2 = nii2.get_fdata()
-    data3 = nii3.get_fdata()
+        nii1 = nib.load(nii1[0])
+        nii2 = nib.load(nii2[0])
+        nii3 = nib.load(nii3[0])
 
-    # Stack the arrays along a new 4th dimension (channel dimension)
-    combined_data = np.stack([data1, data2, data3], axis=-1)
+        # Extract data arrays
+        data1 = nii1.get_fdata()
+        data2 = nii2.get_fdata()
+        data3 = nii3.get_fdata()
 
-    # Create a new NIfTI image
-    combined_nii = nib.Nifti1Image(combined_data, affine=nii1.affine)
+        # Stack the arrays along a new 4th dimension (channel dimension)
+        combined_data = np.stack([data1, data2, data3], axis=-1)
 
-    # Save the combined image
-    mrn = nifti_files.split('_')[-1]
-    output_file = os.path.join(output_dir, uid, uid + '_' + mrn)
-    nib.save(combined_nii, output_file)
+        # Create a new NIfTI image
+        combined_nii = nib.Nifti1Image(combined_data, affine=nii1.affine)
+
+        # Save the combined image
+        output_file = os.path.join(output_dir, uid, f"{uid}.nii.gz")
+        nib.save(combined_nii, output_file)
+
+if __name__ == "__main__":
+    main()
